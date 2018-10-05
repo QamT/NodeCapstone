0c11c9b11b553663a807ee0478667157a39fa34d
Qamar Idrees <qam.idrees@gmail.com>
2018-10-04 23:09:29 -0500

    added auth middleware

--- middleware.js
+++ middleware.js
@@ -1,12 +1,14 @@
 const express = require('express'),
       logger = require('morgan'),
       compression = require('compression'),
-      helmet = require('helmet');
+      helmet = require('helmet')
+      passport = require('passport');
 
 module.exports = app => {
   app.use(compression());
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false })); 
+  app.use(passport.initialize());
   app.use(logger('dev'));
 };
