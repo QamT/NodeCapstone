ae8161c4def3d8cd8ab25b666802970834a4004d
Qamar Idrees <qam.idrees@gmail.com>
2018-10-18 13:58:00 -0500

    added sessions

--- middleware.js
+++ middleware.js
@@ -1,10 +1,21 @@
 const express = require('express'),
       logger = require('morgan'),
-      compression = require('compression'),
+      compression = require('compression')
+      session = require('express-session'),
+      cookieParser = require('cookie-parser'),
+      flash = require('connect-flash'),
       helmet = require('helmet')
       passport = require('passport');
+      
 
 module.exports = app => {
+  app.use(cookieParser(process.env.SECRET));
+  app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(flash());
   app.use(compression());
   app.use(helmet());
   app.use(express.json());
@@ -12,3 +23,4 @@ module.exports = app => {
   app.use(passport.initialize());
   app.use(logger('dev'));
 };
+
