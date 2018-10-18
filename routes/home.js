9740654cf52079ad0a87de102743f0ad7e9e0c15
Qamar Idrees <qam.idrees@gmail.com>
2018-10-18 13:50:23 -0500

    initial commit

---
+++ routes/home.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+      router = express.Router()
+      flash = require('connect-flash');
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Node App'});
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { err: req.flash('error') || '', error: req.flash('err') || '' });
+})
+
+router.get('/login', (req, res) => {
+  res.render('login', { msg: req.flash('success') || '', error: req.flash('error') || '' });
+})
+
+router.get('/logout', (req, res) => {
+  res.cookie('jwt', '');
+  res.redirect('/login');
+})
+
+module.exports = router;
