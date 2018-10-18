0738358d44e04ec59ad307f761b21687bccd8f0f
Qamar Idrees <qam.idrees@gmail.com>
2018-10-18 13:48:15 -0500

    added to hold all input validations

---
+++ validations/userValidation.js
@@ -0,0 +1,15 @@
+const { check, validationResult } = require('express-validator/check');
+
+module.exports = {
+  validateUser: [
+    check('username').isLength({ min: 6, max: 30 }).withMessage('Username must have at least 6 characters')
+      .isAlphanumeric().withMessage('only non-alphanumeric characters allowed').trim().escape(),
+    check('password').isLength({ min: 8, max: 30 }).withMessage('Password must have at least 8 characters')
+      .isAlphanumeric().withMessage('only non-alphanumeric characters allowed'),
+    // .custom((value, {req, local, path}) => {
+    //   return value !== req.body.confirmPassword ? false : value;
+    // }).withMessage("Passwords don't match")
+    check('firstName').isAlpha(),
+    check('lastName').isAlpha()
+  ]
+}
