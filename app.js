1f332007263630dd248f293a69c618963cda8576
Qamar Idrees <qam.idrees@gmail.com>
2018-11-01 02:54:27 -0500

    modularized code

--- app.js
+++ app.js
@@ -33,15 +33,10 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// app.use(function(err, req, res, next) {
-//   // Set locals, only providing error in development
-//   res.locals.message = err.message;
-//   // res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // Render the error page
-//   res.status(err.status || 500);
-//   res.json(err);
-// });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json(err);
+});
 
 let server;
 
