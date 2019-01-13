37cf71cc1b9d320e3b709be09004afef79a1ad34
QamT <36899100+QamT@users.noreply.github.com>
2019-01-13 07:09:15 -0600

    Update userValidation.js

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -3,9 +3,9 @@ const { check, validationResult } = require('express-validator/check');
 module.exports = {
   validateUser: [
     check('username').isLength({ min: 6, max: 30 }).withMessage('Username must have at least 6 characters')
-      .isAlphanumeric().withMessage('only non-alphanumeric characters allowed').trim().escape(),
+      .isAlphanumeric().withMessage('only alphanumeric characters allowed').trim().escape(),
     check('password').isLength({ min: 8, max: 30 }).withMessage('Password must have at least 8 characters')
-      .isAlphanumeric().withMessage('only non-alphanumeric characters allowed'),
+      .isAlphanumeric().withMessage('only alphanumeric characters allowed'),
     // .custom((value, {req, local, path}) => {
     //   return value !== req.body.confirmPassword ? false : value;
     // }).withMessage("Passwords don't match")
